83b336264b44cc76a823ff417dbaf00da38a40e2
MeenaHaira <meenadharmalingam67@gmail.com>
2023-11-21 20:38:40 +0530

    project added

---
+++ src/com/Base/Demo_A.java
@@ -0,0 +1,35 @@
+package com.Base;
+
+public class Demo_A {
+	
+	
+	private void stuName() {
+		System.out.println("Meena");
+		
+
+	}
+	
+	private void stuAge() {
+System.out.println("24");
+	}
+	
+	public static void main(String[] args) {
+		
+		//ClassName refName/objName=new ClassName();
+		
+		Demo_A s= new Demo_A();
+		s.stuAge();
+		s.stuName();
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+
+}
